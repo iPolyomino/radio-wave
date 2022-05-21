975c9278380eb690a764bf6a76f2ef58d3f0fcea
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-05-21 18:56:46 +0900

    add css animation

---
+++ components/StaticBar.tsx
@@ -0,0 +1,7 @@
+const StaticBar: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="#505050" />
+  </svg>
+);
+
+export default StaticBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,10 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import WaveBar from "../components/WaveBar";
+import StaticBar from "../components/StaticBar";
 
 const Home: NextPage = () => {
-  const maxHeight = 1000;
   const barCount = 20;
-
-  const [heights, setHeights] = useState(new Array(barCount));
-  useEffect(() => {
-    setHeights([...heights].map(() => Math.floor(Math.random() * maxHeight)));
-  }, []);
-
-  const [time, setTime] = useState(0);
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime((now) => now + 1);
-      const h = [...heights].map((current) =>
-        Math.floor(current + Math.random() * 20 - 10)
-      );
-      if (!h.includes(NaN)) setHeights(h);
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, [heights]);
 
   return (
     <div className={styles.container}>
@@ -34,10 +16,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1>time : {time}</h1>
         <div className={styles.wavebar}>
-          {heights.map((x, index) => {
-            return <WaveBar key={index.toString()} height={x} />;
+          {Array.from(Array(barCount).keys()).map((index) => {
+            return <StaticBar key={index.toString()} />;
           })}
         </div>
       </main>
